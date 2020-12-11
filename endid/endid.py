100f1325f09d6b72a6b22d88e46632ac986e8f63
Dan Lester <dan@danlester.com>
2020-12-11 11:20:30 +0000

    token and message sent

--- endid/endid.py
+++ endid/endid.py
@@ -1,13 +1,29 @@
 #!python
 
-def call(token='', hostname='endid.app'):
+def call(token='', message='', hostname='endid.app', path='/api'):
     try:
         import http.client as httplib # Python 3
     except:
         import httplib # Python 2.7
 
+    if token == '':
+        print('Please provide a token')
+
     conn = httplib.HTTPSConnection(hostname)
-    conn.request('POST', '/api', token, {"content-type": "application/x-www-form-urlencoded"})
+
+    try:
+        from urllib.parse import urlencode # Python 3
+    except:
+        from urllib import urlencode # Python 2.7
+
+    params = {'token': token}
+
+    if message != '':
+        params['message'] = message
+
+    body = urlencode(params)
+
+    conn.request('POST', path, token, {"content-type": "application/x-www-form-urlencoded"})
     response = conn.getresponse()
     data = response.read()
     
@@ -23,9 +39,13 @@ def cmd():
                     dest='token',
                     help='Endid token from the Slack channel to call')
 
+    parser.add_argument('-m', action='store', default='',
+                    dest='message',
+                    help='Message to display (optional)')
+
     results = parser.parse_args()
 
-    call(token=results.token)
+    call(token=results.token, message=results.message)
 
 if __name__ == "__main__":
     cmd()
