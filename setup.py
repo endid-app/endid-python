9d3c9c43a0cb3f8a6b1a0f0953dcb0e180c8060a
Dan Lester <dan@danlester.com>
2021-02-01 15:49:42 +0000

    v0.0.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="endid",
-    version="0.0.3",
+    version="0.0.4",
     author="Dan Lester",
     author_email="support@endid.app",
     description="Command line utility to call the Endid.app Slack app",
